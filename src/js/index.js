05f3d1b7e286c7a19a4b951cefe6e25c4a80dca0
aschyiel <levy.ulysses@gmail.com>
2014-08-23 18:34:12 -0700

    first.

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+//..src/js/index.js, uly, aug2014..
+
+/*jshint undef:true */
+/*global window */
+
+/**
+* This is the "main" function for the "Angular-Sorter".
+* It kickstarts angular, and everyone else, etc.
+*/
+
+window.aschyiel = window.aschyiel || {};
+(function( ns ) {
+//---
+
+function main() {
+  console.log( "..main.." );
+}
+$( _.once( main ) );
+
+//---
+})( window.aschyiel );
